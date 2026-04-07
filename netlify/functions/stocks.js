d588534161fad93c7f01ae44379d7e02bf388b8e
Ryan <ryan.cstam2772@gmail.com>
2026-04-07 14:49:24 -0400

    Hide API keys - move Finnhub key to env variable

--- netlify/functions/stocks.js
+++ netlify/functions/stocks.js
@@ -1,5 +1,5 @@
 // Using Finnhub free tier - 60 calls/minute, no rate limit issues
-const FINNHUB_KEY = 'd7ajr1hr01qmvlmg84rgd7ajr1hr01qmvlmg84s0';
+const FINNHUB_KEY = process.env.FINNHUB_KEY;
 const cache = {};
 const CACHE_TTL = 5 * 60 * 1000;
 
